0358e14aba25cb7c65524d7e3a7396fa269e6cf9
Igor Andruskiewitsch <i.andruskiewitsch23@gmail.com>
2021-04-02 01:09:19 -0300

    Create empty basic python API

---
+++ torchboard/TorchBoard.py
@@ -0,0 +1,15 @@
+"""
+TorchBoard
+Contains the implementation of the basic API
+"""
+
+class TorchBoard:
+    """ Basic TorchBoard API """
+
+    def __init__(): 
+        self.metrics = [:]
+        pass
+
+    def __setattr__(self, name: str, value: Any):
+        """ Set a new metric to be logged """
+        pass
